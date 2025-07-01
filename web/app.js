9c07fd9963e97cfccf3075fb5b2b7512d8764a9a
Simrun Parulekar <simrun.pro@gmail.com>
2025-07-01 14:25:22 -0700

    Moved index.html to root for GH Pages

---
+++ web/app.js
@@ -0,0 +1,4 @@
+document.getElementById("verifyBtn").addEventListener("click", () => {
+  // This will later trigger the App Clip
+  alert("This will launch the App Clip in the next version!");
+});
